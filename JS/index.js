90ce6a19522500b7ed8ed86896a6dbddda95c8ca
ollaay <olla.anthony@gmail.com>
2018-01-28 22:13:15 +0100

    ajout animation fleche

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
     $('#btn_accueil').on('click', function() { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: 0 }, speed ); // Go
+        return false;
+    });
+
+    $('#btn_fleche').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: ($(page).offset().top-80) }, speed ); // Go
         return false;
     });
 
