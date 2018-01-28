3ae01f7a013cba02f2238386b821dd3b830d50b8
ollaay <olla.anthony@gmail.com>
2018-01-28 21:16:40 +0100

    test fonction js click

--- JS/index.js
+++ JS/index.js
@@ -5,4 +5,32 @@ $(document).ready(function() {
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
         return false;
     });
+
+    $('.btn_Services').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: ($(page).offset().top-80)}, speed ); // Go
+        return false;
+    });
+
+    $('.btn_a_propos').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
+        return false;
+    });
+
+    $('.btn_projets').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
+        return false;
+    });
+
+    $('.btn_contact').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
+        return false;
+    });
 });
