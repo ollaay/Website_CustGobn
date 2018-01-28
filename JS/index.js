7d05aeaf6f4249ae94826fc45ab910919a33919e
ollaay <olla.anthony@gmail.com>
2018-01-28 22:08:38 +0100

    Mise en place animation projet + signature

--- JS/index.js
+++ JS/index.js
@@ -33,4 +33,53 @@ $(document).ready(function() {
         $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
         return false;
     });
+
+    $('#cadre_proj_1').on('mouseover', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_1').style.color='#333';
+        document.getElementById('photo_proj_1').style.opacity='0.3';
+    });
+    $('#cadre_proj_1').on('mouseout', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_1').style.color='transparent';
+        document.getElementById('photo_proj_1').style.opacity='1';
+    });
+
+    $('#cadre_proj_2').on('mouseover', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_2').style.color='#333';
+        document.getElementById('photo_proj_2').style.opacity='0.3';
+    });
+    $('#cadre_proj_2').on('mouseout', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_2').style.color='transparent';
+        document.getElementById('photo_proj_2').style.opacity='1';
+    });
+
+    $('#cadre_proj_3').on('mouseover', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_3').style.color='#333';
+        document.getElementById('photo_proj_3').style.opacity='0.3';
+    });
+    $('#cadre_proj_3').on('mouseout', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_3').style.color='transparent';
+        document.getElementById('photo_proj_3').style.opacity='1';
+    });
+
+    $('#cadre_proj_4').on('mouseover', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_4').style.color='#333';
+        document.getElementById('photo_proj_4').style.opacity='0.3';
+    });
+    $('#cadre_proj_4').on('mouseout', function() { // Au passage de la souri
+        document.getElementById('cadre_proj_4').style.color='transparent';
+        document.getElementById('photo_proj_4').style.opacity='1';
+    });
 });
+
+function initMap() {
+		
+    var uluru = {lat: 48.6501667, lng: 6.209407899999974};
+    var map = new google.maps.Map(document.getElementById('cadre_map'), {
+      zoom: 15,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
