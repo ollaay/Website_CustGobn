a2761604a3b99744f6bd6bf955103edafafacdf8
ollaay <olla.anthony@gmail.com>
2018-01-28 21:18:52 +0100

    finition fonction scroll

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-    $('.btn_accueil').on('click', function() { // Au clic sur un élément
+    $('#btn_accueil').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
-        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        $('html, body').animate( { scrollTop: 0 }, speed ); // Go
         return false;
     });
 
-    $('.btn_Services').on('click', function() { // Au clic sur un élément
+    $('#btn_Services').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: ($(page).offset().top-80)}, speed ); // Go
         return false;
     });
 
-    $('.btn_a_propos').on('click', function() { // Au clic sur un élément
+    $('#btn_a_propos').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
         return false;
     });
 
-    $('.btn_projets').on('click', function() { // Au clic sur un élément
+    $('#btn_projets').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
         return false;
     });
 
-    $('.btn_contact').on('click', function() { // Au clic sur un élément
+    $('#btn_contact').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top-80}, speed ); // Go
